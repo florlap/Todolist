636fea008ba43dd860aa2e74f0a410c36947ab7c
florlap <florencialapadatoff@gmail.com>
2023-10-30 00:06:48 -0300

    CREACION DE FOOTER

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+//import './App.css'
+import './Styles/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
    <h2> iniciando proyecto</h2>,
-   <h3> es una lista de uqehaceres </h3>
-   <h2>Ahora siiiiii a commitearrrr</h2>
+   <h3> es una lista de uqehaceres </h3>,
+   <p>esto es un parrafo</p>
   )
 }
 
---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+function Footer (){
+
+
+    <h2 className="bg-footer"> Buenas, soy el footer de la pagina</h2>
+}
+
+export default Footer;
